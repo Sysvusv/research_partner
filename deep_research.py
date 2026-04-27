d227218e39a83cc9b7d53d72e52949f4ed61a4f8
Sysvusv <141172302+Sysvusv@users.noreply.github.com>
2026-04-27 20:28:39 +0530

    Update deep_research.py

--- deep_research.py
+++ deep_research.py
@@ -19,5 +19,4 @@ with gr.Blocks(theme=gr.themes.Default(primary_hue="sky")) as ui:
     run_button.click(fn=run, inputs=query_textbox, outputs=report)
     query_textbox.submit(fn=run, inputs=query_textbox, outputs=report)
 
-port = int(os.environ.get("PORT", 10000))
-ui.launch(server_name="0.0.0.0", server_port=port)
+ui.launch(server_name="0.0.0.0", server_port=7860)
